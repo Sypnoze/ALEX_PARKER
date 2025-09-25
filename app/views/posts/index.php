c5efeecf6854272d5d6fa24c8b4b32b02260a2b7
Sypnoze <onurkucukkaya.webdev@gmail.com>
2025-09-25 17:01:25 +0200

    Helpers.truncate

--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -11,22 +11,16 @@
             <?php foreach ($posts as $post): ?>
                 <div class="post-title">
                     <a href="single.html">
-                        <h1 <?php echo $post['title'] ?>>
-                            How to make your company website based on bootstrap
-                            framework on localhost?
+                        <h1>
+                            <?php echo $post['title'] ?>
                         </h1>
                     </a>
                 </div>
                 <div class="post-info">
-                    <span <?php echo $post['created_at'] ?>>2016-03-03</span> | <span>Life style</span>
+                    <span><?php echo $post['created_at'] ?></span> | <span>Life style</span>
                 </div>
-                <p <?php echo $post['text'] ?>>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Molestiae ut ratione similique temporibus tempora dicta
-                    soluta? Qui hic, voluptatem nemo quo corporis dignissimos
-                    voluptatum debitis cumque fugiat mollitia quasi quod.
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Molestiae ut ratione similique...
+                <p>
+                    <?php echo \Core\Helpers\truncate($post['text']) ?>
                 </p>
                 <a
                     href="single.html"
--- core/init.php
+++ core/init.php
@@ -7,3 +7,8 @@
 require_once '../app/config/params.php';
 // 2 charger la connexion
 require_once '../core/connection.php';
+
+// 3 charger l'helpers
+require_once '../core/helpers.php';
+
+// 4 charger les constantes 
